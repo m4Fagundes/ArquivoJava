fe3a8c94234989c573743517c74a027f3647efdc
Matheus Fagundes <124789617+m4Fagundes@users.noreply.github.com>
2024-04-05 07:25:03 -0300

    Delete TP1 - Arquivo directory

--- TP1 - Arquivo/TP01 Arquivo/Models/Registro.java
+++
@@ -1,9 +0,0 @@
-public interface Registro extends Comparable<Object>, Cloneable {
-    
-    public int getID();
-    public void setID(int id);
-    public byte[] toByteArray();
-    public void fromByteArray();
-    public int compareTo();
-    
-}
