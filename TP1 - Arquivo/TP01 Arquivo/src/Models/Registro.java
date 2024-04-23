22685b816b1505b783b810b31737f323ae159a60
m4Fagundes <matheusfagundesaraujo@gmail.com>
2024-04-22 21:15:47 -0300

    algumas mudancas nos comentarios

--- TP1 - Arquivo/TP01 Arquivo/src/Models/Registro.java
+++ TP1 - Arquivo/TP01 Arquivo/src/Models/Registro.java
@@ -4,47 +4,33 @@
  * Ela estende Cloneable e é comparável com objetos do tipo Object.
  */
 public interface Registro extends Comparable<Object>, Cloneable {
-    
-    /**
-     * Obtém o ID do registro.
-     * 
-     * @return O ID identificativo do registro.
-     */
-    public int getID();
 
-    /**
-     * Define o ID do registro.
-     * 
-     * @param id O ID a ser atribuído ao registro.
-     */
+    public int getID();
     public void setID(int id);
 
     /**
-     * Serializa o objeto para um array de bytes para armazenamento.
-     * 
-     * @return O array de bytes que representa o registro serializado.
-     * @throws Exception Se houver um erro durante a serialização.
-     */
+    * Serializa o objeto para um array de bytes para armazenamento.
+    * 
+    * @return O array de bytes que representa o registro serializado.
+    */
     public byte[] toByteArray() throws Exception;
 
     /**
-     * Deserializa um array de bytes para obter um objeto Registro.
-     * Este método repopula os atributos do objeto usando dados de um array de bytes.
-     * 
-     * @param ba O array de bytes contendo os dados do registro.
-     * @throws Exception Se houver um erro durante a deserialização.
-     */
-    public void fromByteArray(byte[] ba) throws Exception;
+    * Deserializa um array de bytes para obter um objeto Registro.
+    * Este método repopula os atributos do objeto usando dados de um array de bytes.
+    * 
+    * @param registro O array de bytes contendo os dados do registro.
+    */
+    public void fromByteArray(byte[] registro) throws Exception;
 
     /**
-     * Compara este registro com outro objeto. A implementação desta
-     * função deve lançar ClassCastException se o objeto especificado não
-     * puder ser comparado com este registro.
-     * 
-     * @return Um valor inteiro negativo, zero ou positivo quando este objeto
-     *         é menor que, igual a, ou maior que o objeto especificado, respectivamente.
-     * @throws ClassCastException Se o objeto especificado não puder ser comparado.
-     */
+    * Compara este registro com outro objeto. A implementação desta
+    * função deve lançar ClassCastException se o objeto especificado não
+    * puder ser comparado com este registro.
+    * 
+    * @return Um valor inteiro negativo, zero ou positivo quando este objeto
+    *         é menor que, igual a, ou maior que o objeto especificado, respectivamente.
+    */
     public int compareTo();
 
 }
